f3e01cd68f3c3dc53e36da54fcc3d81ee31be44b
Antonio Reyna Espinoza <antoniore102001@gmail.com>
2024-02-13 22:04:49 -0600

    refactor: remove unnecessary imports

--- ConsultFlow/ConsultFlow.Core.Domain/Entities/IdentityProviders/IdentityProvider.cs
+++ ConsultFlow/ConsultFlow.Core.Domain/Entities/IdentityProviders/IdentityProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsultFlow.Core.Domain.Entities.IdentityProviders
+﻿namespace ConsultFlow.Core.Domain.Entities.IdentityProviders
 {
     public class IdentityProvider
     {
